86b68af58b74a0feda3458a81edf7eb288706d6d
rudal kunwar <ruzalkunwar@gmail.com>
2024-06-29 13:33:46 +0545

    error fixed

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-function encode_message({message, secretKey}) {
-  let msgArray = message.split (' ');
+function encode_message({ message, secretKey }) {
+  if (typeof message !== 'string' || typeof secretKey !== 'string') {
+    throw new Error('Both message and secretKey should be strings.');
+  }
 
-  let msgCodeArray = toCodeArray (msgArray);
-  let keyCodeArray = toCodeArray (secretKey);
+  let msgArray = message.split(' ');
 
-  let encodedCodeArray = toEncodedMessageArray (msgCodeArray, keyCodeArray);
-  let encodedMsgArray = toCharArray (encodedCodeArray);
+  let msgCodeArray = toCodeArray(msgArray);
+  let keyCodeArray = toCodeArray(secretKey);
 
-  return encodedMsgArray;
+  let encodedCodeArray = toEncodedMessageArray(msgCodeArray, keyCodeArray);
+  let encodedMsgArray = toCharArray(encodedCodeArray);
+
+  return encodedMsgArray.join('');
 }
 
-function toCodeArray (arr) {
+function toCodeArray(arr) {
   let newArray = [];
-  let m = arr.toString ();
+  let m = arr.toString();
   for (let i = 0; i < m.length; i++) {
-    if (m[i].match (' ') || m[i].match (',')) {
+    if (m[i].match(' ') || m[i].match(',')) {
       continue;
     } else {
-      let code = toCode (m[i]);
-      newArray.push (code);
+      let code = toCode(m[i]);
+      newArray.push(code);
     }
   }
   return newArray;
 }
 
-function toCode (l) {
-  return l.charCodeAt (0) - 96;
+function toCode(l) {
+  return l.charCodeAt(0) - 96;
 }
 
-function toEncodedMessageArray (arr, keyCode) {
+function toEncodedMessageArray(arr, keyCode) {
   let newArray = [];
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -41,34 +45,40 @@ function toEncodedMessageArray (arr, keyCode) {
     let code = tempMessageCode + tempKeyCode;
 
     if (code > 26) {
-      newArray.push (code - 26 + 96);
+      newArray.push(code - 26 + 96);
     } else {
-      newArray.push (code + 96);
+      newArray.push(code + 96);
     }
     j++;
   }
   return newArray;
 }
 
-function toCharArray (arr) {
+function toCharArray(arr) {
   let newArray = [];
   for (let i = 0; i < arr.length; i++) {
-    newArray.push (String.fromCharCode (arr[i]));
+    newArray.push(String.fromCharCode(arr[i]));
   }
   return newArray;
 }
 
-//for decoding
-function decode_message (msgArray, secretKey) {
-  let msgCodeArray = toCodeArray (msgArray);
-  let keyCodeArray = toCodeArray (secretKey);
+function decode_message({ message, secretKey }) {
+  if (typeof message !== 'string' || typeof secretKey !== 'string') {
+    throw new Error('Both message and secretKey should be strings.');
+  }
 
-  let decodedCodeArray = toDecodedMessageArray (msgCodeArray, keyCodeArray);
-  let decodedMsgArray = toCharArray (decodedCodeArray);
-  return decodedMsgArray;
+  let msgArray = message.split(' ');
+
+  let msgCodeArray = toCodeArray(msgArray);
+  let keyCodeArray = toCodeArray(secretKey);
+
+  let decodedCodeArray = toDecodedMessageArray(msgCodeArray, keyCodeArray);
+  let decodedMsgArray = toCharArray(decodedCodeArray);
+
+  return decodedMsgArray.join('');
 }
 
-function toDecodedMessageArray (arr, keyCode) {
+function toDecodedMessageArray(arr, keyCode) {
   let newArray = [];
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -81,9 +91,9 @@ function toDecodedMessageArray (arr, keyCode) {
     let code = tempMessageCode - tempKeyCode;
 
     if (code <= 0) {
-      newArray.push (code + 26 + 96);
+      newArray.push(code + 26 + 96);
     } else {
-      newArray.push (code + 96);
+      newArray.push(code + 96);
     }
     j++;
   }
