395d921d6f9b96c3255f934666a8e214879814e6
rudal kunwar <ruzalkunwar@gmail.com>
2024-06-29 18:41:31 +0545

    fix : sapce print

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-function encode_message({ message, secretKey }) {
+let space = [];
+function encode_message({message, secretKey}) {
   if (typeof message !== 'string' || typeof secretKey !== 'string') {
-    throw new Error('Both message and secretKey should be strings.');
+    throw new Error ('Both message and secretKey should be strings.');
+  }
+  for (let i = 0; i < message.length; i++) {
+    if (message[i].match (' ')) {
+      space.push (i);
+    }
   }
 
-  let msgArray = message.split(' ');
+  let msgArray = message.split (' ');
 
-  let msgCodeArray = toCodeArray(msgArray);
-  let keyCodeArray = toCodeArray(secretKey);
+  let msgCodeArray = toCodeArray (msgArray);
+  let keyCodeArray = toCodeArray (secretKey);
 
-  let encodedCodeArray = toEncodedMessageArray(msgCodeArray, keyCodeArray);
-  let encodedMsgArray = toCharArray(encodedCodeArray);
+  let encodedCodeArray = toEncodedMessageArray (msgCodeArray, keyCodeArray);
+  let encodedMsgArray = toCharArray (encodedCodeArray, space);
 
-  return encodedMsgArray.join('');
+  return encodedMsgArray.join ('');
 }
 
-function toCodeArray(arr) {
+function toCodeArray (arr) {
   let newArray = [];
-  let m = arr.toString();
+  let m = arr.toString ();
   for (let i = 0; i < m.length; i++) {
-    if (m[i].match(' ') || m[i].match(',')) {
+    if (m[i].match (' ') || m[i].match (',')) {
       continue;
     } else {
-      let code = toCode(m[i]);
-      newArray.push(code);
+      let code = toCode (m[i]);
+      newArray.push (code);
     }
   }
   return newArray;
 }
 
-function toCode(l) {
-  return l.charCodeAt(0) - 96;
+function toCode (l) {
+  return l.charCodeAt (0) - 96;
 }
 
-function toEncodedMessageArray(arr, keyCode) {
+function toEncodedMessageArray (arr, keyCode) {
   let newArray = [];
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -45,40 +51,49 @@ function toEncodedMessageArray(arr, keyCode) {
     let code = tempMessageCode + tempKeyCode;
 
     if (code > 26) {
-      newArray.push(code - 26 + 96);
+      newArray.push (code - 26 + 96);
     } else {
-      newArray.push(code + 96);
+      newArray.push (code + 96);
     }
     j++;
   }
   return newArray;
 }
 
-function toCharArray(arr) {
+function toCharArray (arr) {
   let newArray = [];
+  let counter = 0;
   for (let i = 0; i < arr.length; i++) {
-    newArray.push(String.fromCharCode(arr[i]));
+    for (let j = 0; j < space.length - 1; j++) {
+      if (i + counter === space[j]) {
+        newArray.push (' ');
+        counter++;
+        break;
+      }
+    }
+    newArray.push (String.fromCharCode (arr[i]));
   }
+
   return newArray;
 }
 
-function decode_message({ message, secretKey }) {
+function decode_message({message, secretKey}) {
   if (typeof message !== 'string' || typeof secretKey !== 'string') {
-    throw new Error('Both message and secretKey should be strings.');
+    throw new Error ('Both message and secretKey should be strings.');
   }
 
-  let msgArray = message.split(' ');
+  let msgArray = message.split (' ');
 
-  let msgCodeArray = toCodeArray(msgArray);
-  let keyCodeArray = toCodeArray(secretKey);
+  let msgCodeArray = toCodeArray (msgArray);
+  let keyCodeArray = toCodeArray (secretKey);
 
-  let decodedCodeArray = toDecodedMessageArray(msgCodeArray, keyCodeArray);
-  let decodedMsgArray = toCharArray(decodedCodeArray);
+  let decodedCodeArray = toDecodedMessageArray (msgCodeArray, keyCodeArray);
+  let decodedMsgArray = toCharArray (decodedCodeArray);
 
-  return decodedMsgArray.join('');
+  return decodedMsgArray.join ('');
 }
 
-function toDecodedMessageArray(arr, keyCode) {
+function toDecodedMessageArray (arr, keyCode) {
   let newArray = [];
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -89,11 +104,10 @@ function toDecodedMessageArray(arr, keyCode) {
     let tempMessageCode = arr[i];
 
     let code = tempMessageCode - tempKeyCode;
-
     if (code <= 0) {
-      newArray.push(code + 26 + 96);
+      newArray.push (code + 26 + 96);
     } else {
-      newArray.push(code + 96);
+      newArray.push (code + 96);
     }
     j++;
   }
